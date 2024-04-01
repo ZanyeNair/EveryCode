28b9e2c9b0fdd78aa4d40d3f0f4754473026ac5c
ZanyeNair <zayne.nair@circuitrunners.com>
2024-04-01 19:58:07 -0400

    yay

--- JavaScript/countdownGame/countdown.js
+++ JavaScript/countdownGame/countdown.js
@@ -1,5 +1,7 @@
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
+var vowels = ["A", "E", "I", "O", "U"];
+var consonants = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+var i = 0;
 
 
 
@@ -9,22 +11,50 @@ window.onload = function() {
 }
 
 function setGame() {
-
+    
    
     for (let i = 1; i <= 9; i++) {
         //<div id="1" class="number">1</div>
-        let car = document.createElement("div");
-        car.id = i;
-        car.innerText = letters[Math.floor(Math.random() * 26)];
-        car.classList.add("car");
-        document.getElementById("letters").appendChild(car);
+        let ins = document.createElement("input");
+        ins.id = i;
+        ins.maxLength = 1;
+        ins.classList.add("car");
+        document.getElementById("word").appendChild(ins);
     }
-    let inp = document.createElement("input");
-    document.getElementById("word").appendChild(inp);
+    // let inp = document.createElement("input");
+    // document.getElementById("word").appendChild(inp);
 
 }
 
+function addVowel(){
+    if(i<9){
+        let car = document.createElement("div");
+        car.innerText = vowels[Math.floor(Math.random() * 5)];
+        car.classList.add("car");
+        document.getElementById("letters").appendChild(car);
+        i++;
+    }
+}
+function addConsonants(){
+    if(i<9){
+    let car = document.createElement("div");
+    car.innerText = consonants[Math.floor(Math.random() * 21)];
+    car.classList.add("car");
+    document.getElementById("letters").appendChild(car);
+    i++;
+    }
+}
 
+function afterTimer(){
+    let word = "";
+    for (let i = 1; i <= 9; i++) {
+        const input = document.getElementById(i);
+        word += input.value;
+        input.value = "";
+    }
+    document.getElementById("finalWord").innerHTML = word;
+
+}
 
 function startTimer() {
     var countdownDuration = 30; // Countdown duration in seconds
@@ -40,10 +70,13 @@ function startTimer() {
         } else {
             clearInterval(x); // Stop the countdown when it reaches 0
             document.getElementById("countdown").innerHTML = "FINISHED";
+            afterTimer();
+            
         }
     }
 
     var x = setInterval(updateCountdown, 1000); // Update the countdown every second
+
 }
 
 document.getElementById("startButton").addEventListener("click", startTimer);
