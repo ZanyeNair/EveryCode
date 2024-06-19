d3b70c53c65110b4f79072df408666dd1ebd6710
ZanyeNair <zayne.nair@circuitrunners.com>
2024-06-19 11:59:20 -0400

    sdfdsfgs

--- modeliquiz/src/App.js
+++ modeliquiz/src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom
 import APItest from './APItest';
 import store from '/.store';
 import { Provider } from 'react-redux';
-import { Counter } from './features/counter/Counter';
+import { Counter } from './features/counter/counter';
 
 
 function App() {
