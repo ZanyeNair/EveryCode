9ba463eac7fe6d984e47cbb1f0fde2875f3eacc6
ZanyeNair <zayne.nair@circuitrunners.com>
2024-06-20 23:23:49 -0400

    e

--- modeliquiz/src/redux/rootReducer.js
+++ modeliquiz/src/redux/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import cakeReducer from "./cakes/cakeReducer";
-import iceCreamReducer from "./iceCream/iceCreamReducer";
+import iceCreamReducer from "./icecream/icecreamReducer";
 
 const rootReducer = combineReducers({
     cake:cakeReducer,
